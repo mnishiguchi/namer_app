1736c469ece480c5ea1c375e2221448bab1a85d9
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2024-11-01 15:15:24 +0900

    Change theme

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,8 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 221, 247, 176)),
         ),
         home: MyHomePage(),
       ),
@@ -70,10 +71,21 @@ class BigCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
     return Card(
+      color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Text(pair.asLowerCase),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          // for screen readers to pronounce correctly
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
